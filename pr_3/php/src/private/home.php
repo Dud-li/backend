2a379d0591a6ed832aab335452f035b64f008005
Solovykh Anastasiya <90463325+Dud-li@users.noreply.github.com>
2022-10-12 16:38:44 +0300

    Add files via upload

---
+++ pr_3/php/src/private/home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мультфильмы 2x2</title>
+    <style>
+        h1{
+            text-align: center;
+        }
+        .wrapper{
+            display: flex;
+            justify-content: space-around;
+        }
+        p{
+            font-size: 26px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Мультфильмы 2x2<h1>
+    <div class="wrapper">
+        <div>
+            <a href="#">
+                <img src="../forest.jpg" alt="Битумная гидроизоляция">
+                <p>Атомный лес</p>
+            </a>
+        </div>
+        <div>
+            <a href="#">
+                <img src="../dangerous.jpg" alt="Звукоизоляция">
+                <p>Осторожно, земляне!</p>
+            </a>
+        </div>
+        <div>
+            <a href="#">
+                <img src="../sever.jpg" alt="Стеновые перегородки. Кирпич">
+                <p>Семейный полюс</p>
+            </a>
+        </div>
+    </div>
+
+</body>
+
+</html>
